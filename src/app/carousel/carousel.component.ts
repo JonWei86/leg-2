4a6484ac9451ec9a581024925fe8d1f3131ec7d3
jon <jkwei.86@gmail.com>
2023-10-20 14:08:42 -0400

    added new footer lines, new carousel and refactored code for gallery component

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,14 +1,43 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
-slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+  slides: any[] = new Array(3).fill({
+    id: -1,
+    src: '',
+    title: '',
+    subtitle: '',
+  });
 
-  constructor() { }
+  counter = 0;
+
+  constructor() {}
+
+  onClickPlus(): number {
+    if (this.counter + 1 >= this.slides.length) {
+      console.log(this.counter);
+      return (this.counter = 0);
+    } else {
+      console.log(this.counter);
+
+      return (this.counter += 1);
+    }
+  }
+
+  onClickMinus(): number {
+    if (this.counter === 0) {
+      console.log(this.counter);
+      return (this.counter = this.slides.length - 1);
+    } else {
+      console.log(this.counter);
+
+      return (this.counter -= 1);
+    }
+  }
 
   ngOnInit(): void {
     this.slides[0] = {
@@ -16,9 +45,9 @@ slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
     };
     this.slides[1] = {
       src: './assets/img/2023-05-11.jpg',
-    }
+    };
     this.slides[2] = {
-      src: './assets/img/20181506_234000.jpg',
-    }
-}
+      src: './assets/img/20181206_234000.jpg',
+    };
+  }
 }
